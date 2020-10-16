7a0fdc9dd2851a7f6b94610d9c81f45b0e945c1a
Linkraft <jordan.smiths.games@gmail.com>
2020-10-15 21:11:44 -0400

    Added changes to script

---
+++ Assets/Scripts/ScooterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScooterController : MonoBehaviour
+{
+    public GameObject lHand, rHand, pivot;
+    private Vector3 lPos, rPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lPos = lHand.transform.position;
+        rPos = rHand.transform.position;
+        pivot.transform.position = (lPos + rPos) / 2f;
+        pivot.transform.rotation = rHand.transform.rotation;
+    }
+}
