42a7e9a645582ccb309c0e5ff518a2b01d68fbf2
Smith <linkraft@ufl.edu>
2020-12-15 00:43:03 -0500

    Started Menu Scene

---
+++ Assets/MenuScooterController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MenuScooterController : MonoBehaviour
+{
+    public GameObject lHand, rHand, modelPivot, directionalPivot;
+    private Vector3 lPos, rPos;
+
+    private void Start()
+    {
+        modelPivot.transform.rotation = rHand.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lPos = lHand.transform.position;
+        rPos = rHand.transform.position;
+        modelPivot.transform.position = (lPos + rPos) / 2f;
+        modelPivot.transform.rotation = rHand.transform.rotation;
+    }
+}
